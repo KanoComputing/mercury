d21f7c0ef56c5e496b60b7b17d3161d9c2ef5d95
Albert Casals <skarbat@gmail.com>
2019-07-17 18:24:57 +0000

    Remove redundant voids

--- include/mercury/kw/kw.h
+++ include/mercury/kw/kw.h
@@ -48,11 +48,11 @@ class KanoWorld
     static size_t callback_server_response(void *ptr, size_t size, size_t nmemb, void *user_data);
 
     bool am_i_logged_in(bool verbose);
-    string get_token(void);
-    string get_expiration_date(void);
-    bool save_data(void);
-    bool load_data(void);
-    string whoami(void);
+    string get_token();
+    string get_expiration_date();
+    bool save_data();
+    bool load_data();
+    string whoami();
 
     /**
      * \brief Determine if the user's account has been verified with parental
