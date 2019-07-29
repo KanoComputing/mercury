ad290a4349137c2c0e688267801fcea8a13f0a5b
Tom Bettany <tom.bettany@gmail.com>
2019-07-29 15:56:37 +0100

    Conan: Package Mercury for use with Conan
    Build and package Mercury with Conan to facilitate use from other
    applications.

---
+++ test/test_package/example.cpp
@@ -0,0 +1,15 @@
+/**
+ * \file example.cpp
+ *
+ * \copyright Copyright (C) 2019 Kano Computing Ltd.
+ *            License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
+ *
+ * Simple program to test that Conan packaging and linking is working correctly
+ */
+
+
+#include "mercury/kw/kw.h"
+
+int main() {
+    KanoWorld();
+}
