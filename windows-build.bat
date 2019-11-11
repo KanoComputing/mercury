19635b4a504799a125bf22b30438bfff7a816d9e
Albert Casals <skarbat@gmail.com>
2019-11-11 15:03:13 +0100

    Change to Debug build, invoke msbuild to build & test

--- windows-build.bat
+++ windows-build.bat
@@ -10,12 +10,16 @@
 ::
 
 mkdir build
-mkdir build\release
+mkdir build\debug
 conan remote add dev-server http://dev.kano.me:9300
 conan remote update dev-server http://dev.kano.me:9300
 
-cd build\release
+cd build\debug
 conan install -r dev-server --build=missing --profile=..\..\conan-platforms\conan-profile-Windows-devstudio-2019.info ..\..
 conan install --build=missing --profile=..\..\conan-platforms\conan-profile-Windows-devstudio-2019.info ..\..
 
-cmake -DCMAKE_BUILD_TYPE=release ..\..
+cmake -DCMAKE_BUILD_TYPE=debug ..\..
+
+cd build\debug
+msbuild.exe src\mercury_static.vcxproj
+msbuild.exe test\mercury_gtests.vcxproj
